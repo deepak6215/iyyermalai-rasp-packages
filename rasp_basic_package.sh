b043b3d4827e2fecc1fc85aee8b888deec310ab6
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-12-07 16:49:36 +0530

    Update rasp_basic_package.sh

--- rasp_basic_package.sh
+++ rasp_basic_package.sh
@@ -7,8 +7,3 @@ sudo usermod -aG docker ubuntu
 sudo apt install docker.io -y
 
 
-wget https://github.com/iplon321-cloud/rasp_scripts/raw/main/raspberryData.tar.xz
-
-tar -xJf raspberryData.tar.xz -C /
-
-service cron restart
